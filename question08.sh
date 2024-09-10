5a66ea315609686b5d1f1fce30c3a9a196cceccf
杨佳 <yangjia@Jaylas-MacBook.local>
2024-09-10 20:54:10 +0930

    Add all question scripts from question02 to question18

--- question08.sh
+++ question08.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Create directory dir2
+
+# 1. 创建目录 dir2
 mkdir dir2
-# Move all .txt files to dir2
+
+# 2. 将所有以 .txt 结尾的文件移动到 dir2
 mv *.txt dir2/
-# Stage and commit the changes
-git add dir2
-git commit -m "Move .txt files to dir2 and commit"
+
+# 3. 将更改添加到 Git 暂存区
+git add dir2/*.txt
+
+# 4. 提交更改
+git commit -m "Moved all .txt files to dir2"
+
 
