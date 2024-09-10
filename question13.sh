8aaba6c46d1aaf86cf1ce95e23072427fd320dcc
杨佳 <yangjia@Jaylas-MacBook.local>
2024-09-10 20:59:35 +0930

    Add all question scripts from question02 to question18

--- question13.sh
+++ question13.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# Create a new branch in the submission repository
+
 git checkout -b new-branch
-# Remove all .sh files from this branch
 rm *.sh
 git add .
-git commit -m "Remove .sh files from this branch"
-# Create, add, and commit file13.txt
-echo "This is file13.txt" > file13.txt
+git commit -m "Remove all .sh files"
+echo "This is file13" > file13.txt
 git add file13.txt
-git commit -m "Add file13.txt to new branch"
-# Push the new branch to GitHub
-git push origin new-branch
-
+git commit -m "Add file13.txt"
